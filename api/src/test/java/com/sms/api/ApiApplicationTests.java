2b028872faf9dbf052c22dcb00a82631ce6fd3a9
salimsaid98 <saidhadimu@gmail.com>
2025-08-18 21:52:07 +0300

    initial commit

---
+++ api/src/test/java/com/sms/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sms.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
